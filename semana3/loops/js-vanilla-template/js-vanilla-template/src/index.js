976f429c3185efffa6a019e4ba437ab60afb76fd
laisdejesus <dejesusll@outlook.com>
2021-02-25 16:48:58 -0300

    Exercício aula 13 - Parte 1

---
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,40 @@
+// **Exercícios de interpretação de código**
+
+// Exercício 1
+
+// O código está somando ao valor do 0 ao 5. 
+// O resultado impresso será 0, 1, 3, 6, 10
+
+// Exercício 2
+
+// a. os números maiores que 18: 19, 21, 23, 25, 27, 30
+// b. Não. Porque o for...of não recebe incremento
+
+// Exercício de escrita de código
+// Exercício 3
+
+const arrayOriginal = [5, 24, 35, 98, 74, 63]
+
+// a
+// for (let valores of arrayOriginal) {
+//     console.log ("Valorea", valores)
+// }
+
+// b
+// for (let valores of arrayOriginal) {
+//     console.log ("valores por 10", valores / 10)
+// }
+
+// c
+const arrayPar
+for (let valores of arrayOriginal) {
+    if ((valores / 2) % 0 )) {
+        let par = (valores / 2) % 0 )
+        arrayPar = arrayPar.push(par)
+    }
+}
+
+console.log("Valores pares: ", arrayPar)
+
+
+
