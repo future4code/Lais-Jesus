8f429c2849a29ba17939cd071ce3ad580ef704db
laisdejesus <dejesusll@outlook.com>
2021-02-25 19:04:27 -0300

    Exercicio aula 13 - Parte final

--- semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -26,15 +26,41 @@ const arrayOriginal = [5, 24, 35, 98, 74, 63]
 // }
 
 // c
-const arrayPar
-for (let valores of arrayOriginal) {
-    if ((valores / 2) % 0 )) {
-        let par = (valores / 2) % 0 )
-        arrayPar = arrayPar.push(par)
-    }
-}
+// const arrayOriginal = [5, 24, 35, 98, 74, 63]
+// const arrayPar = []
+// for (let valores of arrayOriginal) {
+//     if (valores % 2 === 0 ) {
+//         arrayPar.push(valores)
+//     }
+//     console.log("Valores pares: ", arrayPar)
+// }
 
-console.log("Valores pares: ", arrayPar)
+// d
+
+// const arrayOriginal = [45,5, 24, 35, 98, 74, 63]
+
+// for (let valores of arrayOriginal) {
+//     let index = arrayOriginal.indexOf(valores)
+//     console.log ("O elemento do índex " + index + " é " + valores)
+// }
+
+
+
+
+// e
+// let maior = arrayOriginal[0]
+// let menor = arrayOriginal[0]
+
+// for (let valores of arrayOriginal) {
+//     if(valores > maior) {
+//         maior = valores
+//     } else if (valores<menor) {
+//         menor = valores
+//     }
+// }
+
+// console.log("O maior numero é " + maior + " e o menor é " + menor)
+
 
 
 
