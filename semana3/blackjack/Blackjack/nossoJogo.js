c7f8bfe73a3b00781265b17e0eb131b1b8d16fcc
laisdejesus <dejesusll@outlook.com>
2021-02-27 15:58:15 -0300

    Adicionando o desafio.js

--- semana3/blackjack/Blackjack/nossoJogo.js
+++ semana3/blackjack/Blackjack/nossoJogo.js
@@ -11,30 +11,30 @@
  * 
  */
 
-console.log ("Bem vindo ao jogo de Blackjack!")
+// console.log ("Bem vindo ao jogo de Blackjack!")
 
-if(confirm("Quer iniciar uma nova rodada?")) {
-   console.log("Vamos começar!")
-} else {
-	console.log("O jogo acabou!")
-}
+// if(confirm("Quer iniciar uma nova rodada?")) {
+//    console.log("Vamos começar!")
+// } else {
+// 	console.log("O jogo acabou!")
+// }
 
-const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-let carta1 = comprarCarta()
-let carta2 = comprarCarta()
-let carta3 = comprarCarta()
-let carta4 = comprarCarta()
+// const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+// let carta1 = comprarCarta()
+// let carta2 = comprarCarta()
+// let carta3 = comprarCarta()
+// let carta4 = comprarCarta()
 
-let resultadoUsuario = carta1.valor + carta2.valor
-let resultadoComputador = carta3.valor + carta4.valor
+// let resultadoUsuario = carta1.valor + carta2.valor
+// let resultadoComputador = carta3.valor + carta4.valor
 
-console.log("Usuário - Cartas: " + carta1.texto +" "+ carta2.texto + " - Pontuação: " + resultadoUsuario) 
-console.log("Computador - Cartas: " + carta3.texto +" "+ carta4.texto + " - Pontuação: " + resultadoComputador) 
+// console.log("Usuário - Cartas: " + carta1.texto +" "+ carta2.texto + " - Pontuação: " + resultadoUsuario) 
+// console.log("Computador - Cartas: " + carta3.texto +" "+ carta4.texto + " - Pontuação: " + resultadoComputador) 
 
-if (resultadoUsuario > resultadoComputador) {
-   console.log ("Você ganhou")
-} else if (resultadoUsuario < resultadoComputador) {
-   console.log ("Você perdeu")
-} else {
-   console.log ("Você empatou")
-}
+// if (resultadoUsuario > resultadoComputador) {
+//    console.log ("Você ganhou")
+// } else if (resultadoUsuario < resultadoComputador) {
+//    console.log ("Você perdeu")
+// } else {
+//    console.log ("Você empatou")
+// }
