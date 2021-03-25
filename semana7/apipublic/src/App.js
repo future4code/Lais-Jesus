f1f86bb1474a424ad9ec386d0ef0d674a377303b
laisdejesus <dejesusll@outlook.com>
2021-03-25 20:16:33 -0300

    Atualizado

--- semana7/apipublic/src/App.js
+++ semana7/apipublic/src/App.js
@@ -1,38 +1,43 @@
-
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import styled from "styled-components"
+
+const Img =styled.img `
+  width: 80%;
+`
 
 export default class App extends React.Component {
  
   state = {
-    dogtorImage : { url: ""},
+    dogtor: {url: ""}
   }
 
-  dogtorDogs = async (event) => {
+  loadNextDogtor = async () => {
+    const response = await axios.get(
+      "https://api.thedogapi.com/v1/images/search", 
+      {
+        headers: {
+          ['x-api-key']: "1dd3be2c-add8-48ca-a329-803d3fa2edf9"
+        }
+      },
+      {params: {limit:1, size:'full'} }
+    );
+    this.setState({ dogtor: response.data[0] });
+    console.log(response.data);
+  };
 
-    try {
-      const appointment = await axios.get (
-        ' https://api.thedogapi.com/v1/images/search'
-      );
-      console.log ('OI');
-      this.setState({dogtorImage: response.data})
-    } catch(error) {
-      console.log(error)
-    }
-  }
- 
- 
   render () {
+    
 
     return (
       <div className="App">
         
         <h1> Doguinho Fofineo, do latim os terapeutas dos humanos </h1>
-        <button onClick = {this.dogtorDogs}>Encontre um Dogtor</button>
+        <button onClick = {this.loadNextDogtor}>Encontre um Dogtor</button>
         <br />
         <br />
-        <img src = {this.state.dogtorImage} alt ={"fotos de doguinhos"} />
+        <Img src = {this.state.dogtor.url} alt ={"fotos de doguinhos"}/>
 
       </div>
     );
