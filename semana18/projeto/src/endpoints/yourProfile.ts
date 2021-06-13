0929fa600c70ab8f904bd6d7972b52eb7946ae60
laisdejesus <dejesusll@outlook.com>
2021-06-13 03:09:17 -0300

    Endpoint Pegar perfil de outro usuário

---
+++ semana18/projeto/src/endpoints/yourProfile.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { userInfo } from "os";
+import connection from "../connection";
+import { getTokenData } from "../services/authenticator";
+
+
+export default async function yourProfile(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+
+    const token = req.headers.authorization as string;
+    const verifiedToken = getTokenData(token);
+    
+
+    if(!verifiedToken){
+        res.statusCode = 403
+        res.statusMessage = "Você não está logado"
+        throw new Error()
+    }
+
+    const result = await connection('cookenu_users')
+     .select('id', 'name', 'email')
+     .where({ id: req.params.id })
+    
+    
+     res.send(result)
+
+   } catch (error) {
+      res.status(400).send({ message: error.message})
+
+   }
+}
--- semana18/projeto/src/index.ts
+++ semana18/projeto/src/index.ts
@@ -2,10 +2,12 @@ import app from "./app"
 import signup from './endpoints/signup'
 import login from "./endpoints/login";
 import myProfile from "./endpoints/myProfile";
+import yourProfile from "./endpoints/yourProfile";
 // import login from "./endpoints/login";
 
 app.post('/signup', signup);
 app.post('/login', login);
 app.get('/user/profile', myProfile);
+app.get('/user/:id', yourProfile);
 
 
